b40f98e4cda0f9f737315454de85ee373293871d
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2021-02-16 21:56:02 +0100

    update tests

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,7 +1,7 @@
-import { fork } from 'child_process'
-import { join } from 'path'
-import { existsSync, rmdirSync } from 'fs'
 import Case from 'case'
+import { fork } from 'child_process'
+import { existsSync, mkdirSync, rmdirSync } from 'fs'
+import { join } from 'path'
 
 const TEST_OUTPUT_DIRNAME = 'test-output'
 const BLOC_NAME = 'test'
@@ -17,17 +17,36 @@ async function run(args: string[]) {
 }
 
 function cleanUpIfNecessary() {
-  const outputPath = TEST_OUTPUT_DIRNAME
-  if (existsSync(outputPath)) {
-    rmdirSync(outputPath, { recursive: true })
+  if (existsSync(TEST_OUTPUT_DIRNAME)) {
+    rmdirSync(TEST_OUTPUT_DIRNAME, { recursive: true })
   }
 }
 
-beforeEach(cleanUpIfNecessary)
+// -----------------------------------------------------------
+
+beforeEach(() => {
+  cleanUpIfNecessary()
+  mkdirSync(TEST_OUTPUT_DIRNAME)
+})
 afterEach(cleanUpIfNecessary)
 
 it('should create folder and files', async () => {
   await run([BLOC_NAME, '--path', TEST_OUTPUT_DIRNAME])
+
+  for (let filename of [
+    'index.js',
+    'TestBloc.js',
+    'TestEvent.js',
+    'TestState.js',
+  ]) {
+    expect(
+      existsSync(join(TEST_OUTPUT_DIRNAME, Case.kebab(BLOC_NAME), filename))
+    ).toBeTruthy()
+  }
+})
+
+it('should support typescript', async () => {
+  await run([BLOC_NAME, '--path', TEST_OUTPUT_DIRNAME, '--typescript'])
 
   for (let filename of [
     'index.ts',
@@ -53,10 +72,10 @@ it('should support alternative file and directory cases', async () => {
   ])
 
   for (let filename of [
-    'index.ts',
-    'test-bloc.ts',
-    'test-event.ts',
-    'test-state.ts',
+    'index.js',
+    'test-bloc.js',
+    'test-event.js',
+    'test-state.js',
   ]) {
     expect(
       existsSync(join(TEST_OUTPUT_DIRNAME, Case.pascal(BLOC_NAME), filename))
